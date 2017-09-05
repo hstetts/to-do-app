e3e36d96f41605d0da68d0eddcb537118763457f
Heidi Stetts <heidistetts@Heidis-MacBook-Air.local>
2017-09-04 21:04:04 -0400

    Revised Assignment 7

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ function onReady(){
   addToDoForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
+    // create a wrapper object
+    let wrapper = document.createElement('div');
+
     // get the text
     let title = newToDoText.value;
 
@@ -15,25 +18,36 @@ function onReady(){
     // create a new input for the checkbox
     let checkbox = document.createElement('input');
 
+    // create a new delete button for new li
+    let deleteButton = document.createElement('button');
+
+    deleteButton.onclick = function(){
+  	this.parentNode.parentNode.innerHTML = '';
+  }
+
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
+    deleteButton.type = 'button';
+    deleteButton.innerHTML = 'X';
+
     // set the title
     newLi.textContent = title;
+    toDoList.appendChild(wrapper);
 
     // attach it to the ul
-    todoList.appendChild(newLi);
+    toDoList.appendChild(newLi);
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
+
+    // attach delete button to the li
+    newLi.appendChild(deleteButton);
 
     //empty the input
     newToDoText.value = '';
   });
 }
-
-addToDoForm.addEventListener('delete', (event) => {
-  event.preventDefault();
 
 window.onload = function() {
   onReady();
