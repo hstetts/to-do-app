a34182e3ce9732c4f328d514b5dd98094999e728
Heidi Stetts <heidistetts@Heidis-MacBook-Air.local>
2017-09-08 09:48:23 -0400

    Assignment 8 completed

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
-function onReady(){
-var toDos = [];
-var addToDoForm = document.getElementById('addToDoForm');
+function onReady() {
+    var toDos = [];
+    var addToDoForm = document.getElementById('addToDoForm');
 
 
-function createNewToDo() {
-  var newToDoText = document.getElementByID('newToDoText');
-  toDos.push({
-    title: newToDoText.value,
-    complete: false
-  });
-  newToDoText.value = '';
-}
+    function createNewToDo() {
+        var newToDoText = document.getElementById('newToDoText');
+        toDos.push({
+            title: newToDoText.value,
+            complete: false
+        });
 
-function renderTheUI(toDos) {
-var toDoList = document.getElementByID('toDoList');
+        newToDoText.value = '';
+        renderTheUI(toDos);
+    }
 
-toDoList.innerHTML = '';
+    function renderTheUI(toDos) {
+        var toDoList = document.getElementById('toDoList');
+        toDoList.innerHTML = '';
+        toDos.forEach(function(toDo) {
+            var newLi = document.createElement('li');
+            var checkbox = document.createElement('input');
+            var deleteButton = document.createElement('button');
+            checkbox.type = "checkbox";
+            deleteButton.type = "button";
+            deleteButton.innerHTML = "Remove";
+            newLi.innerHTML = toDo.title;
+            toDoList.appendChild(newLi);
+            newLi.appendChild(checkbox);
+            newLi.appendChild(deleteButton);
+            deleteButton.onclick = function(){
+              DeleteToDo(this);
+          }
+      });
+  }
 
-toDos.forEach(function(toDo) {
-  var newLi = document.createElement('li');
-  var checkbox = document.createElement('input');
-  checkbox.type = "checkbox";
+      function DeleteToDo(toDo) {
+          toDos.splice(toDo,1);
+          renderTheUI(toDos);
+      }
 
-  newLi.innerHTML = toDo.title;
-
-  toDoList.appendChild(newLi);
-  newLi.appendChild(checkbox);
-});
-
-addToDoForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  createNewToDo();
-});
-
-renderTheUI(todos);
+      addToDoForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            createNewToDo();
+        });
 }
 
 window.onload = function() {
-  onReady();
+    onReady();
 };
